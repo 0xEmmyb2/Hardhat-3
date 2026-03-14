41c1dddf353b76bf4cdd349e98fd9ff3fd4a15eb
Emmy <cyubahiroemmy12@gmail.com>
2026-03-14 16:18:37 +0200

    Updated the savings vault

--- web3/contracts/SavingsVault.sol
+++ web3/contracts/SavingsVault.sol
@@ -3,3 +3,29 @@
 pragma solidity ^0.8.19;
 import "./Ownable.sol";
 
+
+contract SavingsVault is Ownable {
+    uint public totalSavings;
+
+    event Deposit(address indexed sender, uint amount);
+    event Withdrawal(address indexed recipient, uint amount);
+
+    function deposit() public payable {
+        require(msg.value > 0, "You must send some ether to deposit");
+        totalSavings += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw() public onlyOwner {
+        require(totalSavings > 0, "No savings to withdraw");
+        uint amountToWithdraw = totalSavings;
+        totalSavings = 0;
+        (bool success,) = payable(recipient).call{value: totalSavings}("");
+        require(success, "Withdrawal failed");
+        emit Withdrawal(recipient, amountToWithdraw);
+    }
+
+    function getMyBalance() public view returns(uint) {
+        return totalSavings;
+    }
+
