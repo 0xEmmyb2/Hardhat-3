0fd8a19eaa869f15f27454ddc9edaa62c7bf7290
Emmy <cyubahiroemmy12@gmail.com>
2026-03-16 21:03:15 +0200

    Added the ReentrancyGuard import

--- web3/contracts/SavingsVault.sol
+++ web3/contracts/SavingsVault.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contract/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
 
