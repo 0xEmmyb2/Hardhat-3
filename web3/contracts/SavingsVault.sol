2737a3d78796c5f84f915538745018d550ab3cfe
Emmy <cyubahiroemmy12@gmail.com>
2026-03-16 21:00:43 +0200

    Updated the contracts

--- web3/contracts/SavingsVault.sol
+++ web3/contracts/SavingsVault.sol
@@ -1,34 +1,26 @@
 //SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.19;
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contract/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
 
 //Custom Errors
-error NotOwner(address caller);
 error InsufficientBalance(uint requested, uint available);
 error ZeroAmount();
 error TransferFailed();
 error ZeroDeposit();
 
 
-contract SavingsVault is Ownable {
+contract SavingsVault is Ownable, ReentrancyGuard, Pausable {
     mapping(address => uint) public balances;
     uint public totalVaultBalance;
 
     event Deposit(address indexed sender, uint amount);
     event Withdrawal(address indexed recipient, uint amount);
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        if(msg.sender != owner) revert NotOwner(msg.sender);
-        _;
-    }
-
-    
+    constructor() Ownable(msg.sender) {}    
 
     function deposit() public payable {
         if(msg.value == 0) revert ZeroDeposit();
@@ -49,6 +41,14 @@ contract SavingsVault is Ownable {
         (bool success,) = payable(msg.sender).call{value: amount  }("");
         if(!success) revert TransferFailed();
         emit Withdrawal(msg.sender, amount);
+    }
+
+    function pauseVault() public onlyOwner {
+        _pause();
+    }
+
+    function unpauseVault() public onlyOwner {
+        _unpause();
     }
 
     function getMyBalance() public view returns(uint) {
