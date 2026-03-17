4ec95d2422d67ca740dee7bee67b7eb6944e175a
Emmy <cyubahiroemmy12@gmail.com>
2026-03-17 15:28:09 +0200

    Updated the MessageStore

--- web3/contracts/MessageStore.sol
+++ web3/contracts/MessageStore.sol
@@ -3,18 +3,22 @@
 pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//Custom Errors Handling
+error EmptyMessage();
 contract MessageStore is Ownable {
     string public message;
     
 
     event MessageUpdated(string oldMessage, string newMessage);
 
-    constructor (string memory initialMessage) Ownable(msg.sender) {}
+    constructor (string memory initialMessage) Ownable(msg.sender) {
+        message = initialMessage;
+    }
 
 
     //Updating Messages
     function updateMessage(string memory newMessage) public onlyOwner {
-        require(bytes(newMessage).length > 0, "Message cannot be empty");
+        if(bytes(newMessage).length ==0) revert EmptyMessage();
         string memory oldMessage = message;
         message = newMessage;
         emit MessageUpdated(oldMessage, newMessage);
