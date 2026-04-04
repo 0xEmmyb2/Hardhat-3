418b1cac256d337349ba108c11b98a9b22b7e107
0xEmmyb2 <cyubahiroemmy12@gmail.com>
2026-04-04 19:31:33 +0200

    Updated the TodoList

--- web3/contracts/TodoList.sol
+++ web3/contracts/TodoList.sol
@@ -21,18 +21,6 @@ error TodoAlreadyCompleted(uint id);
 
 
 
-//The interfaces of our contract 
-interface ITodoList {
-    event TodoAdded(uint indexed id, string title);
-    event TodoCompleted(uint indexed id);
-
-    function addTodo(string memory _title) external;
-    function completeTodo(uint _id) external;
-    function getAllTodos() external view returns(Todo[] memory);
-    function getTodoById(uint _id) external view returns(uint, string memory, bool);
-    function getTotalTodos() external view returns(uint);
-}
-
 
 contract TodoList is Ownable,ITodoList,Pausable {
     Todo[] public todos;
